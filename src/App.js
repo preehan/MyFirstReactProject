25ffdd603e1537b422c75bbd81a410e59520175f
unknown <preehan056@gmail.com>
2023-06-28 11:25:17 +0500

    MyFirstReactProject

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 
 import { useEffect }from 'react';
-import moviecart from './moviecart';
+import MovieCard from './MovieCard';
 import { useState } from 'react';    
 import './App.css';
 import SearchIcon from './search.svg';
-const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=6d95b806';
+const API_URL = 'http://www.omdbapi.com?apikey=6d95b806';
 const App = () => {
     const [movies, setmovies] = useState([]);
     const [searchTerm, setsearchTerm] = useState('');
@@ -33,7 +33,7 @@ onClick={() => searchmovies(searchTerm)}/>
     ? (
         <div className='container'>
         {movies.map((movie) => 
-       (<moviecart movie = {movie} />) )}
+       (<MovieCard movie = {movie} />) )}
        </div>    
     ) :
     (
